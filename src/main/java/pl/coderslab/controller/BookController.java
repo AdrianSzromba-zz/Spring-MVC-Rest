0b50d428560305c717c39bad4b52bb19c5c107b4
AdrianSzromba <adrian.szromba@gmail.com>
2017-12-15 15:58:33 +0100

    Add next mapping for page

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,13 +1,21 @@
 package pl.coderslab.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import pl.coderslab.bean.MemoryBookService;
 import pl.coderslab.model.Book;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+	
+	@Autowired
+	MemoryBookService mbs;
 
 	@RequestMapping("/hello")
 	public String hello() {
@@ -17,5 +25,10 @@ public class BookController {
 	@RequestMapping("/helloBook")
 	public Book helloBook() {
 		return new Book(1L, "9788324631766", "Thiniking in Java", "Bruce Eckel", "Helion", "programming");
+	}
+	
+	@GetMapping("/")
+	public List<Book> getBooks() {
+		return this.mbs.getList();
 	}
 }
